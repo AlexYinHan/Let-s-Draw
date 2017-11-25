52e592da19e23c67ab081082d420cfd2d08cc5dd
AlexYinHan <411629507@qq.com>
2017-11-25 22:01:12 +0800

    Implement chattingArea in Guess-MainScene

---
+++ Let's Draw/GuessMainSceneViewController.swift
@@ -0,0 +1,81 @@
+//
+//  GuessMainSceneViewController.swift
+//  Let's Draw
+//
+//  Created by apple on 2017/11/25.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+import UIKit
+
+class GuessMainSceneViewController: UIViewController, UITextFieldDelegate {
+
+    //MARK: Properties
+    @IBOutlet weak var chattingInputBoxTextField: UITextField!
+    @IBOutlet weak var chattingDisplayAreaTextView: UITextView!
+    @IBOutlet weak var answerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        chattingInputBoxTextField.delegate = self
+        chattingDisplayAreaTextView.text.append("在这里讨论吧\nf\n\n\n\n\nff\n\nff\n")
+        chattingDisplayAreaTextView.layoutManager.allowsNonContiguousLayout = false
+        answerButton.layer.cornerRadius = 5 //  设置为圆角按钮
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let newChattingRecord = textField.text {
+            chattingDisplayAreaTextView.text.append("\n\(newChattingRecord)")
+            let allStrCount = chattingDisplayAreaTextView.text.count //获取总文字个数
+            chattingDisplayAreaTextView.scrollRangeToVisible(NSMakeRange(0, allStrCount))//把光标位置移到最后
+        }
+        // text field归还FirstResponser地位
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        textField.text = ""
+        return true
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    //MARK: Actions
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        // 按下回答按钮后，弹出一个对话框用于输入答案
+        let answerAlertController = UIAlertController(title: "回答", message: "", preferredStyle: UIAlertControllerStyle.alert)
+        answerAlertController.addTextField(configurationHandler: {
+            (textField:UITextField) -> Void in
+                textField.placeholder = "请输入你的答案"
+            })
+        let cancelAction = UIAlertAction(title: "取消", style: UIAlertActionStyle.cancel, handler: nil)
+        let confirmAction = UIAlertAction(title: "确定", style: UIAlertActionStyle.default, handler: {
+            (UIAlertAction) in
+                // 提交答案
+            if let answer = (answerAlertController.textFields!.first as UITextField?)?.text
+            {
+                print("提交答案\(answer)")
+            }
+            })
+        answerAlertController.addAction(cancelAction)
+        answerAlertController.addAction(confirmAction)
+        self.present(answerAlertController, animated: true, completion: nil)
+    }
+
+}
